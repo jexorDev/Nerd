9d2d5d51b505cfc6e6fda68984cbfa1d89eed69f
Dustin Seger <dseger@askcts.com>
2017-08-10 15:41:42 -0500

    forgot to uncomment line

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const api = require('./routes/api');
-//const DocumentDBClient = require('documentdb').DocumentClient;
+const DocumentDBClient = require('documentdb').DocumentClient;
 const collLink = `dbs/nerd-database/colls/nerd-collection`;
 
 const app = express();
